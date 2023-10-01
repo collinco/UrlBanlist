c8d176e5172565c3efa58b48cdd4d1b9faf26788
Cole Collins <ncollins@utexas.edu>
2023-09-30 22:29:57 -0400

    Track pages that dont reload page. Delete history
    Some site like Youtube dont reload the entire page on navigation. This meant content.js wasnt loading and running the tab closing logic. If we set up a MutationObserver we can watch for page mutations and run the logic then.
    
    - added json file start
    - delete history for page we closed

--- content.js
+++ content.js
@@ -1,5 +1,55 @@
-console.log("Hello from content.js");
-(async () => {
-    const response = await chrome.runtime.sendMessage({ trigger: "hello" });
-    console.log(response);
-})();
+const tempData = {
+  "https://youtube.com/shorts/" : true,
+  "https://www.youtube.com/shorts/" : true,
+};
+
+// Save data to chrome.storage.local
+chrome.storage.local.set({ myData: tempData }, () => {
+  if (chrome.runtime.lastError) {
+    console.error(chrome.runtime.lastError);
+  } else {
+    console.log('Data saved to chrome.storage.local');
+  }
+});
+
+let jsonData = null;
+chrome.storage.local.get(['myData'], (result) => {
+  jsonData = result.myData;
+  if (jsonData) {
+    console.log('JSON data retrieved:', jsonData);
+    observerCallback()
+  } else {
+    console.warn('JSON data not found in storage');
+  }
+});
+
+
+function observerCallback() {
+  // check exact match
+  if (jsonData[window.location.href]) {
+    (async () => {
+        const response = await chrome.runtime.sendMessage({ trigger: window.location.href, url: window.location.href });
+    })();
+    return
+  }
+
+  // check for partial match
+  for (const [key, value] of Object.entries(jsonData)) {
+    if (window.location.href.includes(key)) {
+      (async () => {
+        const response = await chrome.runtime.sendMessage({ trigger: window.location.href, url: window.location.href });
+      })();
+      return
+    }
+  }
+}
+
+function addLocationObserver(callback) {
+  // Options for the observer (which mutations to observe)
+  const config = { attributes: false, childList: true, subtree: false }
+  // Create an observer instance linked to the callback function
+  const observer = new MutationObserver(callback)
+  observer.observe(document.body, config)
+}
+
+addLocationObserver(observerCallback)
--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,13 @@
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
+        console.log("service worker got message" + request.url)
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             if (tabs.length > 0) {
                 chrome.tabs.remove(tabs[0].id);
+                const urlToDelete = request.url;
+                chrome.history.deleteUrl({ url: urlToDelete }, () => {
+                    console.log('History entry deleted:', urlToDelete);
+                });
             }
         });
     }
